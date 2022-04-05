b73128c00314eba2e4fab8b86feaaec672146370
yanayakhontova <99177495+yanayakhontova@users.noreply.github.com>
2022-04-05 10:47:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,84 +4,84 @@
 #include "tstack.h"
 std::string infx2pstfx(std::string inf) {
     std::string c = "";
-    TStack<char, 100> a;
-    std::map<char, int> b;
-    b['('] = 0;
-    b[')'] = 0;
-    b['+'] = 1;
-    b['-'] = 1;
-    b['*'] = 2;
-    b['/'] = 2;
+    TStack<char, 100> st;
+    std::map<char, int> pr;
+    pr['('] = 0;
+    pr[')'] = 0;
+    pr['+'] = 1;
+    pr['-'] = 1;
+    pr['*'] = 2;
+    pr['/'] = 2;
     for (int i = 0; i < inf.size(); i++) {
-        if (b.find(inf[i]) == b.end()) {
+        if (pr.find(inf[i]) == pr.end()) {
             c += inf[i];
             c += ' ';
         } else {
         if (inf[i] == ')') {
-            while (a.Get() != '(') {
-                c += a.Get();
+            while (st.Get() != '(') {
+                c += st.Get();
                 c += ' ';
-                a.pop();
+                st.pop();
             }
-            a.pop();
+            st.pop();
         } else {
-        if (inf[i] == '(' || a.Empty()) {
-            a.push(inf[i]);
+        if (inf[i] == '(' || st.Empty()) {
+            st.push(inf[i]);
         } else {
-            if (b[inf[i]] > b[a.Get()]) {
-            a.push(inf[i]);
+            if (pr[inf[i]] > pr[st.Get()]) {
+            st.push(inf[i]);
             } else {
-                        while (b[inf[i]] <= b[a.Get()]) {
-                            if (!a.Empty()) {
-                            c += a.Get();
+                        while (pr[inf[i]] <= pr[st.Get()]) {
+                            if (!st.Empty()) {
+                            c += st.Get();
                             c += ' ';
-                            a.pop();
+                            st.pop();
                             }
                         }
-                        a.push(inf[i]);
+                        st.push(inf[i]);
                     }
                 }
             }
         }
     }
-    while (!a.Empty()) {
-        c += a.Get();
+    while (!st.Empty()) {
+        c += st.Get();
         c += ' ';
-        a.pop();
+        st.pop();
     }
     c.pop_back();
     return c;
 }
-int eval(std::string br) {
-    TStack<int, 100> a;
-    int x1, x2, r, i = 0;
-    while (i < br.size()) {
-        if (br[i] >= '0') {
-            a.push(br[i] - '0'); 
+int eval(std::string pref) {
+    TStack<int, 100> st;
+    int b1, b2, r, i = 0;
+    while (i < pref.size()) {
+        if (pref[i] >= '0') {
+            st.push(pref[i] - '0'); 
         } else {
-            if (br[i] != ' ') {
-                x2 = a.Get();
-                a.pop();
-                x1 = a.Get();
-                a.pop();
-                switch (br[i]) {
+            if (pref[i] != ' ') {
+                b2 = st.Get();
+                st.pop();
+                b1 = st.Get();
+                st.pop();
+                switch (pref[i]) {
                     case '+':
-                        r = x1 + x2;
+                        r = b1 + b2;
                         break;
                     case '-':
-                        r = x1 - x2;
+                        r = b1 - b2;
                         break;
                     case '*':
-                        r = x1 * x2;
+                        r = b1 * b2;
                         break;
                     case '/':
-                        r = x1 / x2;
+                        r = b1 / b2;
                         break;
                     }
-                a.push(r);
+                st.push(r);
             }
         }
         i++;
     }
-    return a.Get();
+    return st.Get();
 }
