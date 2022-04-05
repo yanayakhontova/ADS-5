fb0189c5ec1016cfd2cbd1cd34a4256d799080e4
yanayakhontova <99177495+yanayakhontova@users.noreply.github.com>
2022-04-05 11:01:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,87 +1,113 @@
 // Copyright 2021 NNTU-CS
 #include <string>
 #include <map>
+#include <stack>
 #include "tstack.h"
+int prior(char w) {
+  switch (w) {
+    case '(':
+      return 0;
+    case ')':
+      return 1;
+    case '+':
+      return 2;
+    case '-':
+      return 2;
+    case '*':
+      return 3;
+    case '/':
+      return 3;
+  }
+  return 0;
+}
+bool search(std::string c, char w) {
+  for (int i = 0; i < c.length(); ++i) {
+    if (c[i] == w) {
+      return true;
+    }
+  }
+  return false;
+}
+
 std::string infx2pstfx(std::string inf) {
-    std::string c = "";
-    TStack<char, 100> st;
-    std::map<char, int> pr;
-    pr['('] = 0;
-    pr[')'] = 0;
-    pr['+'] = 1;
-    pr['-'] = 1;
-    pr['*'] = 2;
-    pr['/'] = 2;
-    for (int i = 0; i < inf.size(); i++) {
-        if (pr.find(inf[i]) == pr.end()) {
-            c += inf[i];
-            c += ' ';
-        } else {
-        if (inf[i] == ')') {
-            while (st.Get() != '(') {
-                c += st.Get();
-                c += ' ';
-                st.pop();
-            }
-            st.pop();
-        } else {
-        if (inf[i] == '(' || st.Empty()) {
-            st.push(inf[i]);
-        } else {
-            if (pr[inf[i]] > pr[st.Get()]) {
-            st.push(inf[i]);
-            } else {
-                        while (pr[inf[i]] <= pr[st.Get()]) {
-                            if (!st.Empty()) {
-                            c += st.Get();
-                            c += ' ';
-                            st.pop();
-                            }
-                        }
-                        st.push(inf[i]);
-                    }
-                }
-            }
+  TStack<char, 100> stack;
+  std::stack<char> Stack;
+  std::string number = "0123456789";
+  std::string operation = "()+-*/";
+  std::string res = "";
+  for (int i = 0; i < inf.length(); ++i) {
+    if (search(number, inf[i])) {
+      res += inf[i];
+      res += ' ';
+    } else if (search(operation, inf[i])) {
+      if (prior(inf[i]) == 0) {
+        Stack.push(inf[i]);
+      } else if (Stack.empty()) {
+        Stack.push(inf[i]);
+      } else if (prior(inf[i]) > prior(Stack.top())) {
+        Stack.push(inf[i]);
+      } else if (inf[i] == ')') {
+        while (Stack.top() != '(') {
+          res += Stack.top();
+          res += ' ';
+          Stack.pop();
         }
+        if (Stack.top() == '(') {
+          Stack.pop();
+        }
+      } else {
+        for (int j = 0; j<= Stack.size(); ++j) {
+          if (prior(Stack.top()) >= prior(inf[i])) {
+            res += Stack.top();
+            res += ' ';
+            Stack.pop();
+          }
+        }
+        Stack.push(inf[i]);
+      }
     }
-    while (!st.Empty()) {
-        c += st.Get();
-        c += ' ';
-        st.pop();
+  }
+  if (!Stack.empty()) {
+    for (int j = 0; j <= Stack.size(); ++j) {
+      res += Stack.top();
+      res += ' ';
+      Stack.pop();
     }
-    c.pop_back();
-    return c;
+  }
+  if (res[res.length() - 1] == ' ') {
+    res.erase(res.length() - 1);
+  }
+  return res;
 }
 int eval(std::string pref) {
-    TStack<int, 100> st;
-    int b1, b2, r, i = 0;
-    while (i < pref.size()) {
-        if (pref[i] >= '0') {
-            st.push(pref[i] - '0'); 
-        } else {
-            if (pref[i] != ' ') {
-                b2 = st.Get();
-                st.pop();
-                b1 = st.Get();
-                st.pop();
-                switch (pref[i]) {
-                    case '+':
-                        r = b1 + b2;
-                        break;
-                    case '-':
-                        r = b1 - b2;
-                        break;
-                    case '*':
-                        r = b1 * b2;
-                        break;
-                    case '/':
-                        r = b1 / b2;
-                        break;
-                    }
-                st.push(r);
-            }
-        }
-        i++;
+  int summ = 0;
+  std::stack<int> Stack;
+  TStack<int, 100> stack;
+  std::string number = "0123456789";
+  std::string operation = "()+-*/";
+  for (int i = 0; i < pref.length(); ++i) {
+    if (search(number, pref[i])) {
+      Stack.push(pref[i] - '0');
+    } else if (search(operation, pref[i])) {
+      int a = 0, b = 0;
+      a = Stack.top();
+      Stack.pop();
+      b = Stack.top();
+      Stack.pop();
+      if (pref[i] == '+') {
+        Stack.push(b + a);
+      }
+      if (pref[i] == '-') {
+        Stack.push(b - a);
+      }
+      if (pref[i] == '*') {
+        Stack.push(b * a);
+      }
+      if (pref[i] == '/') {
+        Stack.push(b / a);
+      }
     }
-    return st.Get();
+  }
+  summ = Stack.top();
+  return summ;
 }
